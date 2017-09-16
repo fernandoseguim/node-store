7bd53a57873ad978cf25037e1cab72a005779e4c
Fernando Seguim <fernando.seguim@gmail.com>
2017-09-16 20:58:24 -0300

    added connection with mongo db

--- src/app.js
+++ src/app.js
@@ -2,9 +2,13 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 const router = express.Router();
+
+//database connection
+mongoose.connect('mongodb://seguim:2316@ds036967.mlab.com:36967/mongo-nodestore');
 
 // load routes
 const indexRoute = require('./routes/index-route');
